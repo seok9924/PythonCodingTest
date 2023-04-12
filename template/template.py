a72fd3194385411861c3a63c62f5d09b5d34706b
SeiHyunChoi <41310927+seok9924@users.noreply.github.com>
2023-04-12 17:31:33 +0900

    init

---
+++ template/template.py
@@ -0,0 +1,19 @@
+import math
+import sys
+from os import path
+import itertools
+import collections
+import heapq
+import functools
+import re
+import bisect
+from collections import deque
+import os
+
+if path.exists('input.txt'):
+    sys.stdin = open("input.txt", "r")
+
+
+a= list(map(int,input().split()))
+b= int(input())
+
