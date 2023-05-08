1319f8386d25b165b29917f7f82c4f1e7e4d63df
SeiHyunChoi <41310927+seok9924@users.noreply.github.com>
2023-05-08 18:01:21 +0900

    class3 아기상어
    직접 다시 구현해보기

--- BoJ/class3/16236.py
+++ BoJ/class3/16236.py
@@ -5,47 +5,41 @@ if path.exists('input.txt'):
 
 from collections import  deque
 n=int(input())
-graph=[]
-for _ in range(n):
-    graph.append(list(map(int,input().split())))
-
-dx=[0,0,1,-1]
-dy=[-1,1,0,0]
-
-time=0
+graph=[list(map(int,input().split()))for _ in range(3)]
+print(graph)
 
 x,y,size=0,0,2
-
 for i in range(n):
     for j in range(n):
         if graph[i][j]==9:
-            x=i
-            y=j
-
+            x,y=i,j
 
 def eat(x,y,size):
-    distance=[[0]*n for _ in range(n)]
-    visited=[[0]* n for _ in range(n)]
     q=deque()
+    visted=[[0]*n for _ in range(n)]
+    distance=[[0]*n for _ in range(n)]
+    dx=[1,-1,0,0]
+    dy=[0,0,1,-1]
     q.append((x,y))
-    visited[x][y]=1
+    visted[x][y]=1
     temp=[]
-
     while q:
         curx,cury=q.popleft()
 
         for i in range(4):
             nx=curx+dx[i]
             ny=cury+dy[i]
-
-            if 0<=nx<n and 0<=ny<n and visited[nx][ny]==0:
+            if 0<=nx<n and 0<=ny<n and visted[nx][ny]==0:
                 if graph[nx][ny]<=size:
                     q.append((nx,ny))
-                    visited[nx][ny]=1
+                    visted[nx][ny]=1
                     distance[nx][ny]=distance[curx][cury]+1
-                if graph[nx][ny]<size and graph[nx][ny]!=0:
+
+                if graph[nx][ny]!=0 and graph[nx][ny]<size:
                     temp.append((nx,ny,distance[nx][ny]))
+
     return sorted(temp,key=lambda x:(-x[2],-x[0],-x[1]))
+
 
 
 result=0
@@ -65,3 +59,5 @@ while True:
         time=0
 
 print(result)
+
+
